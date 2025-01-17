ac480c9f3bf942b480d60942456de7c0b08090e8
daraakitoyemru <dakit711@mtroyal.ca>
2025-01-16 19:56:21 -0700

    fixing cors error

--- backend/image-api.js
+++ backend/image-api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 const assetPath = path.join(__dirname, "assets");
 
 app.use("/assets", express.static(assetPath));
+app.use(cors({ origin: "https://your-frontend-domain.com" }));
 
 app.get("/images", (req, res) => {
   fs.readdir(assetPath, (error, filename) => {
