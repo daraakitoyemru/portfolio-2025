c5b52348c00cb92ef5b65b5dc21efcbecfc73a18
daraakitoyemru <dakit711@mtroyal.ca>
2025-01-16 19:08:48 -0700

    fixed somethings

--- backend/image-api.js
+++ backend/image-api.js
@@ -7,13 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const assetPath = path.join(__dirname, "assets");
-const filenames = fs.readdirSync(assetPath);
-filenames.forEach((file) => console.log(file));
 
 app.use("/assets", express.static(assetPath));
 
 app.get("/images", (req, res) => {
-  res.json(filenames);
+  fs.readdir(assetPath, (error, filename) => {
+    if (error) {
+      console.log(error);
+    }
+    res.json(filename);
+  });
 });
 
 app.listen(PORT, () => {
